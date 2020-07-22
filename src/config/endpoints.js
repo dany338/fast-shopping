c9b5ec94cbd08f6504689a85b984fd086b9f5ad2
Daniel Andres Gallo Garcia <dgallo@teravisiontech.com>
2020-07-21 19:34:55 -0500

    first commit - config folder & libraries

---
+++ src/config/endpoints.js
@@ -0,0 +1,30 @@
+export default {
+  users: {
+    get: '',
+    list: '',
+    search: '',
+    create: '',
+    update: '',
+  },
+  categories: {
+    get: '',
+    list: '',
+    search: '',
+    create: '',
+    update: '',
+  },
+  products: {
+    get: '',
+    list: '',
+    search: '',
+    create: '',
+    update: '',
+  },
+  orders: {
+    get: '',
+    list: '',
+    search: '',
+    create: '',
+    update: '',
+  },
+};
